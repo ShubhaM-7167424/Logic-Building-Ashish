33c697c7b629d18754c537d7e310f301a8b23d59
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-01-03 19:43:05 +0530

    sum of two numbers

---
+++ SumOfTwoNumbers.js
@@ -0,0 +1,10 @@
+function sum(a,b ){
+    const sum = a + b;
+    console.log('sum ', sum);
+}
+
+console.log(sum(38, 12));
+console.log(sum(-38, 12));
+console.log(sum(-3, -2));
+console.log(sum(-3, -2.5));
+console.log(sum(3.5, -2.5));
