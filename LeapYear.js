12950d304b022df2273d17efbee50cfbe1890991
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-01-16 12:28:39 +0530

    Leap Year

---
+++ LeapYear.js
@@ -0,0 +1,13 @@
+function isLeapYear(year){
+    if(year % 4 === 0){
+        return 'Leap Year'
+    }else{
+        return 'Non Leap Year'
+    }
+}
+
+
+
+console.log(isLeapYear(1999));
+console.log(isLeapYear(2004));
+console.log(isLeapYear(1697));
