59b9cc497eeffd2619b38079381b6e8a6a70a5b8
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-01-23 21:04:10 +0530

    updated

--- Factorial.js
+++ Factorial.js
@@ -1,16 +1,25 @@
-function factorial(number){
-  let fact = 1;
-  for(i= number; i>0; i--){
-    fact = fact * i;
-  }
-  return fact;
+// function factorial(number){
+//   let fact = 1;
+//   for(i= number; i>0; i--){
+//     fact = fact * i;
+//   }
+//   return fact;
 
+// }
+
+// console.log(factorial(5));
+// console.log(factorial(7));
+// console.log(factorial(1));
+// console.log(factorial(10));
+
+function Factorial(number) {
+  let factorial = 1;
+  for(let i=number; i>0; i--){
+    factorial = factorial * i;
+  }
+  return factorial
 }
 
-console.log(factorial(5));
-console.log(factorial(7));
-console.log(factorial(1));
-console.log(factorial(10));
-
-
-
+console.log(Factorial(1));
+console.log(Factorial(0));
+console.log(Factorial(6));
--- LeapYear.js
+++ LeapYear.js
@@ -11,3 +11,5 @@ function isLeapYear(year){
 console.log(isLeapYear(1999));
 console.log(isLeapYear(2004));
 console.log(isLeapYear(1697));
+
+
