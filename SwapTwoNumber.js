2c85859650ba6381e1d0ee85a2a186669664527a
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-02-03 23:22:22 +0530

    more methods

--- SwapTwoNumber.js
+++ SwapTwoNumber.js
@@ -6,3 +6,45 @@ function swapNumbers(a, b) {
   console.log(a, b);
 }
 console.log(swapNumbers(1, 2));
+
+function swapNumbers2(a, b) {
+  console.log(a, b);
+  a = a + b;
+  b = a - b;
+  a = a - b;
+  console.log(a, b);
+}
+console.log(swapNumbers2(5, 2));
+
+function swapNumbers3(a, b) {
+  console.log(a, b);
+  a = a * b;
+  b = a / b;
+  a = a / b;
+  console.log(a, b);
+}
+console.log(swapNumbers3(15, 3));
+
+function swapNumbers4(a, b) {
+  console.log(a, b);
+  a = a ^ b;
+  b = a ^ b;
+  a = a ^ b;
+  console.log(a, b);
+}
+console.log(swapNumbers4(25, 5));
+
+function swapNumbers5(a, b) {
+  console.log(a, b);
+  a = a | b;
+  b = a & b;
+  console.log(a, b);
+}
+console.log(swapNumbers5(10, 2));
+
+function swapNumbers6(a, b) {
+  console.log(a, b);
+   [a, b] = [b, a];
+  console.log(a, b);
+}
+console.log(swapNumbers6(20, 5));
