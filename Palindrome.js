a7f5dfa26004f884486ec57fae183105bb91262d
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-02-04 17:27:51 +0530

    adding check for non string values

--- Palindrome.js
+++ Palindrome.js
@@ -1,4 +1,8 @@
 function isPalindrome(str) {
+  if (typeof str !== "string") {
+    throw new Error("Only Strings are allowed");
+  }
+
   for (let i = 0, j = str.length - 1; i < j; i++, j--) {
     if (str[i] !== str[j]) {
       return false;
@@ -11,3 +15,4 @@ console.log(isPalindrome("racecar"));
 console.log(isPalindrome("malayalam"));
 console.log(isPalindrome("madam"));
 console.log(isPalindrome("rafting"));
+console.log(isPalindrome(479));
