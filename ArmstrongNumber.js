e0fafb1d0f86ea5c72155cf633c6d5ce010dabda
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-02-05 21:53:52 +0530

    check Armstrong Number

---
+++ ArmstrongNumber.js
@@ -0,0 +1,21 @@
+function isArmstrongNumber(number) {
+    let num = number;
+    let sum = 0;
+    while(number > 0){
+        let r = number % 10;
+        sum += r * r * r;
+        number = Math.floor(number / 10);
+    }
+    if(num == sum){
+        return 'Armstrong';
+    }else{
+        return 'Not Armstrong';
+    }
+    
+}
+
+console.log(isArmstrongNumber(153));
+console.log(isArmstrongNumber(239));   
+console.log(isArmstrongNumber(407));   
+console.log(isArmstrongNumber(370));   
+console.log(isArmstrongNumber(567));   
