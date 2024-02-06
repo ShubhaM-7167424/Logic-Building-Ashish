b69b74f120de5a241d78b3b4699c97c17226f972
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-02-06 20:21:10 +0530

    longest word and its length in a string

--- FindLongestWordInASentence.js
+++ FindLongestWordInASentence.js
@@ -1,12 +1,14 @@
 function findLongestWordInASentence(str) {
   let words = str.split(" ");
   let maxLength = 0;
+  let longestWord = "";
   for (let i = 0; i < words.length; i++) {
     if (words[i].length > maxLength) {
       maxLength = words[i].length;
+      longestWord = words[i];
     }
   }
-  return maxLength;
+  return {maxLength, longestWord};
 }
 
 console.log(findLongestWordInASentence("I have a car"));
