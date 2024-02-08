7fe0c9cdf1c26292c05abbf16d01e5997754cedd
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-02-08 22:43:28 +0530

    longest length added

--- FindLongestWordInASentence.js
+++ FindLongestWordInASentence.js
@@ -1,14 +1,15 @@
 function findLongestWordInASentence(str) {
   let words = str.split(" ");
-  let maxLength = 0;
-  let longestWord = "";
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].length > maxLength) {
-      maxLength = words[i].length;
+  let longestLength = words[0].length;
+  let longestWord = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length > longestLength) {
+      longestLength = words[i].length;
       longestWord = words[i];
     }
   }
-  return {maxLength, longestWord};
+  return { maxLength: longestLength, longestWord };
 }
 
 console.log(findLongestWordInASentence("I have a car"));
