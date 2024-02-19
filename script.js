17824a860eff748f903fe0235fe6ef6d450d4ab2
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-02-19 10:11:40 +0530

    new sample 2

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-function countCharacter(str) {
-    str = str.toLowerCase()
-    let result={}
+function isPalindrome(str) {
+  if (typeof str !== "string") {
+    throw new Error("Only Strings are allowed");
+  }
 
-    for(let i=0; i<str.length; i++){
-       
-        if(!result[str[i]]){
-            result[str[i]]=0
-        }
-        result[str[i]] += 1;
+  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+    if (str[i] !== str[j]) {
+      return false;
     }
-    return result;
+  }
+  return true;
 }
-console.log(countCharacter('Shubham singh'));
 
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("malayalam"));
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("rafting"));
+
+function isPalindrome2(str) {
+  return str === str.split("").reverse().join("");
+}
+
+console.log(isPalindrome2("rahul"));
+console.log(isPalindrome2("racecar"));
+console.log(isPalindrome2("malayalam"));
