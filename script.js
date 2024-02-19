9522cf5b42d55edbf05f704ed6edb4f801c2af5d
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-02-19 10:17:01 +0530

    new sample 3

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-function isPalindrome(str) {
-  if (typeof str !== "string") {
-    throw new Error("Only Strings are allowed");
-  }
+function smallestDigitInANumber(number) {
+  let smallestDigit = 9;
 
-  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
-    if (str[i] !== str[j]) {
-      return false;
+  while (number > 0) {
+    let r = number % 10;
+    if (r < smallestDigit) {
+      smallestDigit = r;
     }
+    number = Math.floor(number / 10);
   }
-  return true;
+
+  return smallestDigit;
 }
 
-console.log(isPalindrome("racecar"));
-console.log(isPalindrome("malayalam"));
-console.log(isPalindrome("madam"));
-console.log(isPalindrome("rafting"));
-
-function isPalindrome2(str) {
-  return str === str.split("").reverse().join("");
-}
-
-console.log(isPalindrome2("rahul"));
-console.log(isPalindrome2("racecar"));
-console.log(isPalindrome2("malayalam"));
+console.log(smallestDigitInANumber(3418));
+console.log(smallestDigitInANumber(3052));
